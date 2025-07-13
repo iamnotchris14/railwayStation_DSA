3087c086b8a176d0236db90165fff02cff8d084d
Hashika <hkeshreen30@gmail.com>
2025-07-14 00:33:21 +0800

    Updated putrajayaLine, main.cpp, and project workspace

--- CodeNexus_RailwaySystem/main.cpp
+++ CodeNexus_RailwaySystem/main.cpp
@@ -1,5 +1,26 @@
 //
 // Created by Christopher Lee Jia Yung on 12/07/2025.
 //
-#include "lines.h"
+#include "putrajayaLine.h"
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    initializePutrajayaLine();
+    cout << "Putrajaya Line: " << endl;
+
+    // display
+    for (int i = 0; i < putrajayaLine.stationsCount; ++i) {
+        cout << putrajayaLine.stations[i].name;
+
+        if (i < putrajayaLine.stationsCount - 1) {
+            int fromIndex = putrajayaLine.stations[i].index;
+            int toIndex = putrajayaLine.stations[i + 1].index;
+            int travelTime = graph[fromIndex][toIndex];
+
+            cout << " -" << travelTime << "min-> ";
+        }
+    }
+    cout << endl;
+}
